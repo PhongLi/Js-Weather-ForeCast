46074d209325bcf883acea5c65fc45d9332dbd64
phongli <longphitran1109@gmail.com>
2023-02-13 22:29:01 +0700

    fix chart.js. toggle active for forecast card

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { dateToWords, findIcon, setInnerText } from './utils.js';
 import { createChart } from './tempChart.js';
-
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
@@ -15,6 +14,9 @@ const weatherText = $('#weather-text');
 const pressureEl = $('#pressure-data');
 const windEl = $('#wind-data');
 const weather = $('.weather-details');
+const mode = $('.mode');
+let isDay = true;
+let currentIndex = 0;
 
 // Format the time and date
 const time = new Date().toLocaleTimeString('en-us', {
@@ -32,14 +34,17 @@ const date = new Date().toLocaleDateString('en-us', {
 
 function getWeatherData(value) {
     const city_name = value ?? city.value;
+    city.value = '';
     if (city_name) {
         const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city_name}&days=3&aqi=no&alerts=yes`;
         fetch(url)
             .then((res) => res.json())
             .then((data) => {
-                renderWeather(data);
-                renderDayForecast(data);
-                handleEvents(data);
+                if (data) {
+                    renderWeather(data);
+                    renderDayForecast(data);
+                    handleEvents(data);
+                }
             })
             .catch((err) => {
                 console.log(err.message);
@@ -84,16 +89,58 @@ function renderDayForecast(data) {
 
 function handleEvents(data) {
     const forecastDays = data?.forecast?.forecastday;
+    activeCard();
     //handle event click on day forecast
     weather.onclick = (event) => {
         if (event.target.classList.contains('forecast')) {
             const index = event.target.getAttribute('data-index');
             createChart(forecastDays[index]);
+            currentIndex = index;
+            activeCard();
         }
     };
+    mode.onclick = () => {
+        isDay = !isDay;
+        changeBg(isDay);
+    };
 }
+function getWeatherWithLocation() {
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+            // get weather data with current location in the first time
+            getWeatherData(`${lat} ${lon}`);
+        },
+        (err) => console.log(err),
+    );
+}
+function activeCard() {
+    const forecastNodeList = $$('.forecast');
+    const activeNode = $('.forecast.active');
+    if (activeNode) {
+        activeNode.classList.remove('active');
+    }
+    forecastNodeList.forEach((fNode) => {
+        if (fNode.dataset.index == currentIndex) {
+            fNode.classList.add('active');
+        }
+    });
+}
+function changeBg(isDay) {
+    if (document.body) {
+        const hour = new Date().getHours();
+        isDay = isDay ?? (hour >= 6 && hour < 18);
+        document.body.style = `background-image:  linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.1)), url(${
+            isDay ? './assets/images/cloud.jpg' : './assets/images/night.jpg'
+        });`;
+    }
+}
+
 window.onload = () => {
-    getWeatherData('VietNam');
+    getWeatherWithLocation();
+    changeBg();
+    activeCard();
 };
 
 form.onsubmit = (e) => {
